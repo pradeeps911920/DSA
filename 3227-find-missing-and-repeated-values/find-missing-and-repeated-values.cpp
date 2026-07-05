0be788eaca297e1a3be4f6057702374e3c46917f
Pradeep Sain <pradeepsain8456@gmail.com>
2026-07-05 22:50:10 +0530

    Time: 14 ms (39.10%) | Memory: 31.4 MB (50.30%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,23 +2,50 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<int> freq(n*n+1 , 0);
-        for(int i  = 0 ; i< n ; i++){
-            for(int j = 0; j< n ; j++ ){
-                freq[grid[i][j]]++;
+        // vector<int> freq(n*n+1 , 0);
+        // for(int i  = 0 ; i< n ; i++){
+        //     for(int j = 0; j< n ; j++ ){
+        //         freq[grid[i][j]]++;
+        //     }
+        // }
+        // int repeated = -1 , missing = -1;
+        // for(int i = 1; i <=n*n ; i++){
+        //     if(freq[i]==2 ){
+        //         repeated = i;
+
+        //     }
+        //     if(freq[i]==0){
+        //         missing = i;
+        //     }
+        // }
+        // return {repeated , missing };
+        unordered_set<int> s;
+        vector<int> ans;
+        int a , b;
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j< n ; j++){
+                if(s.find(grid[i][j])!= s.end()){
+                    a = grid[i][j];
+                    ans.push_back(a);
+                    break;
+                }else{
+                    s.insert(grid[i][j]);
+
+                }
             }
         }
-        int repeated = -1 , missing = -1;
-        for(int i = 1; i <=n*n ; i++){
-            if(freq[i]==2 ){
-                repeated = i;
+        int expSum = n*n*(n*n+1)/2;
+        int aSum = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0; j < n ; j++){
+                aSum+=grid[i][j];
 
             }
-            if(freq[i]==0){
-                missing = i;
-            }
+            
         }
-        return {repeated , missing };
+        b = expSum + a - aSum ;
+        ans.push_back(b);
+        return ans;
         
     }
 };
