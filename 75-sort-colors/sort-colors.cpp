d51346c048fd868153e4ee0cb44e51a6c25d28af
Pradeep Sain <pradeepsain8456@gmail.com>
2026-09-07 22:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (79.32%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -19,3 +19,25 @@ public:
         
     }
 };
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int st = 0, end = nums.size()-1;
+//         while(st<=end){
+//             int mid = st + (end-st)/2;
+//             if(nums[mid] == 0 ){
+//                 swap(nums[mid] , nums[st]);
+//                 st++;
+//             }else if(nums[mid]== 2){
+//                 swap(nums[mid] , nums[end]);
+//                 end--;
+//             }else{
+//                 st++;
+//                 end--;
+//             }
+//         }
+//         return;
+
+        
+//     }
+// };
